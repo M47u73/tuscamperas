e8addb77943ec560b507616368118117f377aa87
M47u73 <alacamperita@gmail.com>
2026-04-29 01:11:24 -0300

    Delete tienda_ropa/build.sh

--- tienda_ropa/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Script de build para Render.com
-# Configurar en Render: Build Command = ./build.sh
-
-set -o errexit
-
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
